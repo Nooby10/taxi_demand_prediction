c4fe7691dc75dae9bf975a487b7c8aa0718681c2
Ridwan Yusuf <yusufridwanayodeji@gmail.com>
2025-04-28 10:59:39 -0400

    fix the API key not found error

--- src/config.py
+++ src/config.py
@@ -7,9 +7,8 @@ HOPSWORK_PROJECT_NAME = 'taxi_demand_service'
 try:
     HOPSWORK_API_KEY = os.environ['HOPSWORK_API_KEY']
 except Exception:
-    print('There is an error loading the API key. It is now being reloaded')
-    load_dotenv(PARENT_DIR / '.env')
-    HOPSWORK_API_KEY = os.environ['HOPSWORK_API_KEY']
+    print('Create a .env file in the project root with the Hopsworks API key')
+
     
 FEATURE_GROUP_NAME = 'time_series_data_hourly_feature_group'
 FEATURE_GROUP_VERSION = '1'
